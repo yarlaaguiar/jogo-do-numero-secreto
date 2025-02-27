77b3b334e742045513dae97aa0b03b1a8ad160f9
yarlaaguiar <yarla.aguiar@gmail.com>
2025-02-27 14:03:39 -0300

    Deixando o jogo mais fácil2

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 a 100');  
+    exibirTextoNaTela('p', 'Escolha um número entre 1 a 50');  
 }
 exibirMensagemInicial();
 function verificarChute() {

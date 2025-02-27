edabdacaa31a13881c40548cf4a0005d752f294b
yarlaaguiar <yarla.aguiar@gmail.com>
2025-02-27 13:29:41 -0300

    alteração número limite

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 //let paragrafo = document.querySelector('p');
 //paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -22,7 +22,7 @@ function exibirTextoNaTela(tag, texto) {
 }
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 a 3');  
+    exibirTextoNaTela('p', 'Escolha um número entre 1 a 100');  
 }
 exibirMensagemInicial();
 function verificarChute() {
